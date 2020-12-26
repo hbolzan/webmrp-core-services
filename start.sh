87147a717ac17b68c05f61f99c2a6fa28ab8fda7
Henrique Bolzan <henrique@bolzan.com.br>
2020-12-26 13:38:28 -0300

    Include legacy service in services startup script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nameko run --config bff/config.yaml bff.service data-hub.service forms.service
+nameko run --config bff/config.yaml bff.service data-hub.service forms.service legacy.service
