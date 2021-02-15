1b924a4b95c00c2551a3c671284ac4194ade25c0
Henrique Bolzan <henrique@bolzan.com.br>
2021-02-15 19:04:39 -0300

    Fixes and new queries

--- legacy/legacy_resources.py
+++ legacy/legacy_resources.py
@@ -1,3 +1,5 @@
 resources_index = {
-    "fornecedores": {"source": "fornecedores.query.sql", "singular": "fornecedor", "pk": "id"},
+    "fornecedores": {"source": "fornecedores.select.sql", "singular": "fornecedor", "pk": "frn.id"},
+    "paises": {"source": "nfebase.paises", "singular": "país", "pk": "codigo"},
+    "municipios": {"source": "nfebase.ibge_municipios_reduzida", "singular": "municipio", "pk": "municipiocodigo"},
 }
---
+++ legacy/resources/sql/municipios.select.sql
@@ -0,0 +1,6 @@
+select
+  municipiocodigo as codigo, municipionome as nome
+from
+  nfebase.ibge_municipios_reduzida
+{where}
+{order_by}
