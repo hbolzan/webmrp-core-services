273d4b6dd0c141e936723ba78494778f7d5ec8ad
Henrique Bolzan <henrique@bolzan.com.br>
2021-02-18 06:17:11 -0300

    Add filter params to legacy query endpoint

--- legacy/resources/sql/municipios.select.sql
+++ legacy/resources/sql/municipios.select.sql
@@ -1,6 +1,13 @@
 select
-  municipiocodigo as codigo, municipionome as nome
-from
-  nfebase.ibge_municipios_reduzida
+  codigo, nome, uf
+from (
+  select
+    mun.municipiocodigo as codigo, mun.municipionome as nome, ufs.ufsigla as uf
+  from
+    nfebase.ibge_municipios_reduzida mun
+  join
+    nfebase.ibge_ufs ufs
+    on ufs.ufcodigo = mun.ufcodigo
+  ) q1
 {where}
 {order_by}
--- legacy/tests/test_query.py
+++ legacy/tests/test_query.py
@@ -19,11 +19,42 @@ class BuildQueryTests(unittest.TestCase):
         self.assertEqual(query.default_select("items", "x > 1", ""), "select * from items where x > 1")
         self.assertEqual(query.default_select("items", "", "z"), "select * from items order by z")
 
-    def test_params_to_search_where(self):
-        self.assertEqual(query.params_to_search_where({}), "")
+    def test_params_to_filter(self):
+        self.assertEqual(query.params_to_filter({}), "")
         self.assertEqual(
-            query.params_to_search_where({"searchValue": "xyz", "searchFields": "a,b,c"}),
+            query.params_to_filter({"filterBy": "uf", "filterWith": "SP"}),
+            "cast(uf as varchar) = 'SP'"
+        )
+        self.assertEqual(
+            query.params_to_filter({"filterBy": "uf", "filterWith": "RJ"}),
+            "cast(uf as varchar) = 'RJ'"
+        )
+        self.assertEqual(
+            query.params_to_filter({"filterBy": "pais", "filterWith": "1058"}),
+            "cast(pais as varchar) = '1058'"
+        )
+        self.assertEqual(
+            query.params_to_filter({"filterBy": "pais,uf", "filterWith": "1058,SP"}),
+            "cast(pais as varchar) = '1058' and cast(uf as varchar) = 'SP'"
+        )
+
+    def test_params_to_search_condition(self):
+        self.assertEqual(query.params_to_search_condition({}), "")
+        self.assertEqual(
+            query.params_to_search_condition({"searchValue": "xyz", "searchFields": "a,b,c"}),
             "cast(a as varchar) ilike '%xyz%' or cast(b as varchar) ilike '%xyz%' or cast(c as varchar) ilike '%xyz%'"
+        )
+        self.assertEqual(
+            query.params_to_search_condition(
+                {"searchValue": "xyz", "searchFields": "b,c", "filterBy": "uf", "filterWith": "SP"}
+            ),
+            "(cast(uf as varchar) = 'SP') and (cast(b as varchar) ilike '%xyz%' or cast(c as varchar) ilike '%xyz%')"
+        )
+        self.assertEqual(
+            query.params_to_search_condition(
+                {"filterBy": "uf", "filterWith": "SP"}
+            ),
+            "cast(uf as varchar) = 'SP'"
         )
 
     def test_from_index(self):
